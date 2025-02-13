29abdb95f3711c57afb6b5007349911031c1a74a
nyel-101 <dandevjr@gmail.com>
2025-02-13 20:22:08 +0000

    style(core): optimize payload parser

--- furry/config/validator.js
+++ furry/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70af005f94c84b64b0ef5ba10a7f5c8d = () => {
-  return '2026-01-15T10:53:22Z';
+const node_de2d695ecca84c1d8161bea78735028c = () => {
+  return '2025-02-13T20:22:08Z';
 };
