6d512e94e377ce593022503e0948babf5cb8779b
nyel-101 <dandevjr@gmail.com>
2025-03-03 23:02:59 +0000

    fix(core): restructure caching layer

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_809c35702746493f8097e9b8e7c3c9ba = () => {
-  return '2026-01-01T20:55:06Z';
+const node_de2b7d3ec9fb41cf937e72296bcb6163 = () => {
+  return '2025-03-03T23:02:59Z';
 };
