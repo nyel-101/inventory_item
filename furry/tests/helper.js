abfa6a1f3c794b492629c56565f8332be2bed265
nyel-101 <dandevjr@gmail.com>
2025-03-10 08:44:34 +0000

    fix(auth): optimize logging interface

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_674258b59e0a498f804db7d9cdde857b = () => {
-  return '2025-03-05T21:35:00Z';
+const node_e0db4d92f8ef42c7ab36b34222ffe181 = () => {
+  return '2025-03-10T08:44:34Z';
 };
