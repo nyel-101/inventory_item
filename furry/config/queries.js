1b8f44b6269ee98e19afab1a97e9c05cd423f73e
nyel-101 <dandevjr@gmail.com>
2025-03-20 01:07:03 +0000

    style(config): optimize auth tokens

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b530945c5c2b48a7aaedae49ad0010ee = () => {
-  return '2026-06-23T18:07:59Z';
+const node_376238cf12f74833b7c28806f24a4ec8 = () => {
+  return '2025-03-20T01:07:03Z';
 };
