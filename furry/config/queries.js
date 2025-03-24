15688d7537692f0dc8124b001e0dd5c2515ba5bb
nyel-101 <dandevjr@gmail.com>
2025-03-24 13:34:55 +0000

    feat(worker): fix middleware routes

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_376238cf12f74833b7c28806f24a4ec8 = () => {
-  return '2025-03-20T01:07:03Z';
+const node_4f759f7f132b473ca2ca24d8aa35cb33 = () => {
+  return '2025-03-24T13:34:55Z';
 };
