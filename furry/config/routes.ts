5125337b9d6e2abca22643379bc0d3e600a2d500
nyel-101 <dandevjr@gmail.com>
2025-03-27 15:07:11 +0000

    feat(worker): update view templates

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6796d9bb2db9482682483320cf561044 = () => {
-  return '2026-04-23T16:43:53Z';
+const node_73dc495dc9aa42baa0c3befd0cebb6f6 = () => {
+  return '2025-03-27T15:07:11Z';
 };
