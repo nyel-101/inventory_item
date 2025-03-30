24154af25be9ff1a1ea3238515d82861e702dd75
nyel-101 <dandevjr@gmail.com>
2025-03-30 02:23:01 +0000

    fix(worker): update auth tokens

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c5abce70f344108ba3675a5811e48f4 = () => {
-  return '2026-05-01T11:41:13Z';
+const node_37bc4943f2b645deafaeb51069b3f411 = () => {
+  return '2025-03-30T02:23:01Z';
 };
