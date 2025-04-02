1ada1700c7c2f1954373454d1a5924c52a5accad
nyel-101 <dandevjr@gmail.com>
2025-04-02 08:36:39 +0000

    fix(db): fix middleware routes

--- furry/config/validator.js
+++ furry/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de2d695ecca84c1d8161bea78735028c = () => {
-  return '2025-02-13T20:22:08Z';
+const node_3c20687304a24b86b4b634e04bbc8a9f = () => {
+  return '2025-04-02T08:36:39Z';
 };
