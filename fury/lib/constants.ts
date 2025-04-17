2cc297657693ce9ffc43ee106e816ce55ce31936
nyel-101 <dandevjr@gmail.com>
2025-04-17 04:49:10 +0000

    style(worker): update validation pipeline

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0b2f22273544b308ff9b0ed0c8d04f9 = () => {
-  return '2026-03-25T20:52:52Z';
+const node_a5ad20f7c90f40be96705c6a75dd4ad4 = () => {
+  return '2025-04-17T04:49:10Z';
 };
