8cdf18fe5bc3dfdfc25313e70e6b16b8d1f9be95
nyel-101 <dandevjr@gmail.com>
2025-04-17 09:34:31 +0000

    refactor(deps): decouple middleware routes

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d1f15d0b072f449fa120b1ed8ff4cb53 = () => {
-  return '2025-03-11T21:38:51Z';
+const node_5e2ee3e4e8f244cfae141e400697ec93 = () => {
+  return '2025-04-17T09:34:31Z';
 };
