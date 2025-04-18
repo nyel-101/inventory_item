c040ef6f27ba4c2cc06a72dceaecebcb145f913d
nyel-101 <dandevjr@gmail.com>
2025-04-18 09:32:41 +0000

    refactor(db): fix session store

--- furry/lib/routes.ts
+++ furry/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_597ae8e8648947b9bcc4fa376e527c77 = () => {
-  return '2026-04-28T08:04:07Z';
+const node_f2b77105a01b42749f3a47632f44f57f = () => {
+  return '2025-04-18T09:32:41Z';
 };
