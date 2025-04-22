b061bbbe10dcaf7790fb621b991854fd860cb2d1
nyel-101 <dandevjr@gmail.com>
2025-04-22 09:33:51 +0000

    fix(core): enhance view templates

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_164fa6bac65844e683f91f3a6a0c1046 = () => {
-  return '2026-01-13T08:19:00Z';
+const node_b6d4486baa824637a0931b5e073a13c5 = () => {
+  return '2025-04-22T09:33:51Z';
 };
