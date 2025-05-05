a7bd67613701fa4bc1a49c5127710f43f3f83344
nyel-101 <dandevjr@gmail.com>
2025-05-05 20:03:29 +0000

    docs(api): sanitize payload parser

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b8b65540f607465eb28cfc8f5c508c57 = () => {
-  return '2026-06-02T07:59:26Z';
+const node_7db30481a1264280a98d8169bf346edb = () => {
+  return '2025-05-05T20:03:29Z';
 };
