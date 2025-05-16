9ac55831c1c20d1ef146a148aebe2c7a900cf3e6
nyel-101 <dandevjr@gmail.com>
2025-05-16 12:38:36 +0000

    refactor(deps): sanitize payload parser

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_635ee79c2ff5460ba3058096d61214fe = () => {
-  return '2025-04-03T07:37:17Z';
+const node_22689df9559d4b1898a31971e3bfe286 = () => {
+  return '2025-05-16T12:38:36Z';
 };
