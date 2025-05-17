a05e236ad75031f269d8aea84ff9bdf7c087b31a
nyel-101 <dandevjr@gmail.com>
2025-05-17 20:06:20 +0000

    test(config): restructure middleware routes

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f2cb612748f9471192d52bc0d9d068a6 = () => {
-  return '2025-05-02T21:50:39Z';
+const node_e4166aab7136446b870ee9f4ef072b86 = () => {
+  return '2025-05-17T20:06:20Z';
 };
