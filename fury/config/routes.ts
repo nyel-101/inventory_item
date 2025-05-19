983c9949aee2887491985a629cb023c61fbb1101
nyel-101 <dandevjr@gmail.com>
2025-05-19 08:41:27 +0000

    chore(db): sanitize auth tokens

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef116bc27c2c415eb17fdbda2b7b7044 = () => {
-  return '2025-04-29T18:19:25Z';
+const node_6c5e6f0d30ae4d3599e74ced42c494c2 = () => {
+  return '2025-05-19T08:41:27Z';
 };
