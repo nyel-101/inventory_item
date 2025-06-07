ad6be6070d002939d535fd5f3935f2c4bcb94aba
nyel-101 <dandevjr@gmail.com>
2025-06-07 10:08:45 +0000

    docs(db): refactor middleware routes

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_72a82bbf76f34dc6b3f93acc87e7ed5d = () => {
-  return '2026-05-25T22:33:03Z';
+const node_4a02cc463ab54763ab7ee35e5ff3dc73 = () => {
+  return '2025-06-07T10:08:45Z';
 };
