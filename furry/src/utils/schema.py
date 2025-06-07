649ae5adc6c06fb7b111e9c6cda879f65c0af7cb
nyel-101 <dandevjr@gmail.com>
2025-06-07 18:45:13 +0000

    refactor(api): decouple session store

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77283431fa3f492cbe5bd48e034aa0a1 = () => {
-  return '2025-05-05T00:31:06Z';
+const node_caf56c82f01a4ea68cec40ade9d09d16 = () => {
+  return '2025-06-07T18:45:13Z';
 };
