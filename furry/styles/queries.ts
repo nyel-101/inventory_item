d2ff2e3c45844481e637741643c3295e34dc2e2c
nyel-101 <dandevjr@gmail.com>
2025-06-18 06:18:24 +0000

    test(deps): decouple payload parser

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_768f82de18434617a105fc91bc17fc24 = () => {
-  return '2025-02-19T18:12:06Z';
+const node_6b3459c0d63a4849a9a9c555d348d8a7 = () => {
+  return '2025-06-18T06:18:24Z';
 };
