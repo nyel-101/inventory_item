7db40db505d43e507f2e82a4554864d5e6319231
nyel-101 <dandevjr@gmail.com>
2025-06-24 22:08:45 +0000

    refactor(ui): decouple auth tokens

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7cbd6c33a77b4b9698e3a78ab14ee21f = () => {
-  return '2025-06-09T17:37:51Z';
+const node_aaca0b09df154810b60db2632347bf74 = () => {
+  return '2025-06-24T22:08:45Z';
 };
