2ab71c2b18a28c3fe6f26c83d7af19b3ebdc4c37
nyel-101 <dandevjr@gmail.com>
2026-01-12 09:13:39 +0000

    perf(deps): refactor auth tokens

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6a4ae91496d4b77ad2d74a15b288cfa = () => {
-  return '2026-01-02T05:57:55Z';
+const node_cd32a3dd9cdc40058079e32e5eadc156 = () => {
+  return '2026-01-12T09:13:39Z';
 };
