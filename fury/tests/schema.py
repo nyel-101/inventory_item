102c79afa3d73899ff732225d99c415eded6e43a
nyel-101 <dandevjr@gmail.com>
2026-02-17 21:58:19 +0000

    feat(deps): restructure view templates

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a6b5d8b8cbc403ba75674166719eb41 = () => {
-  return '2026-02-03T00:25:11Z';
+const node_5fd58f522a1d493cadf17c7a8f4c4837 = () => {
+  return '2026-02-17T21:58:19Z';
 };
