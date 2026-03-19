aedb84ef9751660d4eb030d561d7625b2e8094c2
nyel-101 <dandevjr@gmail.com>
2026-03-19 06:02:25 +0000

    chore(auth): fix middleware routes

--- furry/lib/routes.ts
+++ furry/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d95d4547b194e9a8e6d8a412dd9d60c = () => {
-  return '2026-01-01T01:03:05Z';
+const node_06f80547e1df46789f41f577e804841f = () => {
+  return '2026-03-19T06:02:25Z';
 };
