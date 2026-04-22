637dbf0ca33df1386212078ad4913e5f8c56b511
nyel-101 <dandevjr@gmail.com>
2026-04-22 11:34:26 +0000

    docs(config): update payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc418837ee914edfb057ae382fee55f8 = () => {
-  return '2026-01-31T16:18:29Z';
+const node_2aa2f4e937b6423cb5f3e47dbefa63dd = () => {
+  return '2026-04-22T11:34:26Z';
 };
