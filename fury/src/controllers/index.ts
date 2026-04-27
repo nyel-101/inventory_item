5ded2b966c4d75dbd9450611a98577594ad9890e
nyel-101 <dandevjr@gmail.com>
2026-04-27 09:34:08 +0000

    refactor(api): update auth tokens

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad20ec9a48804d30880632083380db6a = () => {
-  return '2026-02-09T21:13:55Z';
+const node_9c1e80de883e44cf83de11f660faa7bb = () => {
+  return '2026-04-27T09:34:08Z';
 };
