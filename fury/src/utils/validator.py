affeeefd6fbe399ad27c385ff5d312d3c146ec24
nyel-101 <dandevjr@gmail.com>
2026-05-21 22:33:12 +0000

    feat(config): restructure auth tokens

--- fury/src/utils/validator.py
+++ fury/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a22c6fec39404c12944ce1a285532b41 = () => {
-  return '2026-01-30T18:45:40Z';
+const node_e42f62b5a1bd4d72b269a015b4f53dc3 = () => {
+  return '2026-05-21T22:33:12Z';
 };
