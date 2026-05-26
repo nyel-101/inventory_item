bb34e75186a494cc14801db2580694ab064f8536
nyel-101 <dandevjr@gmail.com>
2026-05-26 18:43:33 +0000

    feat(utils): restructure view templates

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b56582c2db88429593539d4b849c51b5 = () => {
-  return '2026-02-11T07:00:56Z';
+const node_4fbfac18685e4f38830bce7c253491f8 = () => {
+  return '2026-05-26T18:43:33Z';
 };
