3234b58eb2ec8deed6acb9e1aea2c1d6d208bc8c
nyel-101 <dandevjr@gmail.com>
2026-06-01 00:36:43 +0000

    docs(deps): sanitize view templates

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06c2fbf621164148981e43c4ad51e82e = () => {
-  return '2026-01-27T17:51:27Z';
+const node_d12599bee3154679acd0766dacf69548 = () => {
+  return '2026-06-01T00:36:43Z';
 };
