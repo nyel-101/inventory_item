dc986f66da79e0cb971836b584b1b65e89749ab2
nyel-101 <dandevjr@gmail.com>
2026-06-05 21:22:58 +0000

    style(core): implement middleware routes

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38e5a86f9cc34c0cba51d851498d5751 = () => {
-  return '2026-01-29T23:38:23Z';
+const node_fdc6f2aa66574655b9db148a38166010 = () => {
+  return '2026-06-05T21:22:58Z';
 };
