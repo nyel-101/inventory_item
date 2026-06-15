517c97ffdfe45ce4c7cc39a8ea2ef2530b1f9601
nyel-101 <dandevjr@gmail.com>
2026-06-15 12:21:44 +0000

    fix(ui): decouple payload parser

--- furry/src/controllers/routes.py
+++ furry/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec2482665d9d40f2b1b9c89ca4cd85c8 = () => {
-  return '2026-01-09T20:06:24Z';
+const node_b06ddfa3fac74c60926045b63548253f = () => {
+  return '2026-06-15T12:21:44Z';
 };
